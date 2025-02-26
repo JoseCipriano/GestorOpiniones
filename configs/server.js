b29ca1ad8ad34d440fa820220648d47511db92cd
josecipriano <ciprianoaceytunoj@gmail.com>
2025-02-25 23:13:25 -0600

    Configs

---
+++ configs/server.js
@@ -0,0 +1,66 @@
+'use strict';
+
+import express from 'express';
+import cors from 'cors';
+import helmet from 'helmet';
+import morgan from 'morgan';
+import {dbConnection} from './mongo.js'
+import limiter from '../src/middlewares/validar-cant-peticiones.js';
+import authRoutes from '../src/auth/auth.routes.js'
+import userRoutes from '../src/users/user.routes.js'
+import publicationRoutes from '../src/publication/publication.routes.js'
+import categoryRoutes from '../src/category/category.routes.js';
+
+
+
+
+
+const configurarMiddlewares = (app) => {
+    app.use(express.urlencoded({extended: false}))
+    app.use(cors())
+    app.use(express.json())
+    app.use(helmet()) 
+    app.use(morgan('dev'));
+    app.use(limiter);
+
+}
+
+const configurarRutas = (app) => {
+
+    app.use("/gestorOpiniones/v1/auth", authRoutes);
+    app.use("/gestorOpiniones/v1/users", userRoutes);
+    app.use("/gestorOpiniones/v1/publication", publicationRoutes);
+    app.use("/gestorOpiniones/v1/category" , categoryRoutes)
+
+
+
+}
+
+const conectarDB = async () => {
+    try {
+        await dbConnection();
+        console.log("Conexion a la base de datos exitosa");
+    } catch (error) {
+        console.error('Error conectado a la base de datos', error);
+        process.exit(1);
+        
+    }
+
+
+}
+
+ export const iniciarServidor = async () => {
+    const app = express() ;
+    const port =  process.env.PORT || 3001;
+
+    await conectarDB();
+
+    configurarMiddlewares(app);
+    configurarRutas(app);
+
+    app.listen(port, () => {
+        console.log(`Server running in port ${port}`);
+    })
+
+
+}
